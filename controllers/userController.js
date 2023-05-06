a4a4511150dcf9d8c77522d4403cf7bf34926778
NavinduAmerasinghe <navinduamerasinghe@gmail.com>
2023-05-06 19:03:02 +0530

    user signin created

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,3 +24,43 @@ exports.signup = async (req, res, next) => {
     });
   }
 };
+
+exports.signin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      // return next(new ErrorResponse('E-mail and password are required', 400))
+      return res.status(400).json({
+        success: false,
+        message: "E-mail and password are required",
+      });
+    }
+
+    //check user e-mail
+    const user = await User.findOne({ email });
+    if (!user) {
+      // return next(new ErrorResponse('Invalid credentials', 400))
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    //verify user password
+    const isMatched = await user.comparePassword(password);
+    if (!isMatched) {
+      return res.status(400).json({
+        success: false,
+        message: "Inavlid credentials",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+
+    next(new ErrorResponse("Cannot log in, check your credentials", 400));
+  }
+};
